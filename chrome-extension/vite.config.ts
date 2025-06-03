ee611ae6939a277a39cccd15649fec7ab3196c7a
reiyi-lai <rylai0219@gmail.com>
2025-06-03 15:14:52 -0400

    Updated vite config for chrome extension

--- chrome-extension/vite.config.ts
+++ chrome-extension/vite.config.ts
@@ -5,18 +5,22 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        background: 'src/background.ts',
-        content: 'src/content.ts',
-        popup: 'src/popup/index.html',
+        background: path.resolve(__dirname, 'src/background.ts'),
+        content: path.resolve(__dirname, 'src/content.ts'),
+        // popup: path.resolve(__dirname, 'src/popup/index.html'),
+        // 'popup-script': path.resolve(__dirname, 'src/popup/popup.ts'),
       },
       output: {
         entryFileNames: 'js/[name].js',
-        chunkFileNames: 'js/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        format: 'es',
       },
+      external: ['chrome']
     },
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: process.env.NODE_ENV === 'development',
   },
   resolve: {
     alias: {
